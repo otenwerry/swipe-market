7e69ed2d7a2811ee1dfb59bbb58756092024349c
otenwerry <otenwerry@gmail.com>
2025-03-21 14:04:51 +0000

    link to delete listing sent in emai

--- app.py
+++ app.py
@@ -499,7 +499,8 @@ def send_connection_email():
       f"As a reminder, {buyer_name} wants to be swiped into {buyer_listing.dining_hall} "
       f"between {start_time_formatted} and {end_time_formatted} for ${price_str}. "
       f"They can pay via {buyer_listing.payment_methods}.\n\n"
-      f"{buyer_name}, remember to delete your listing once you've agreed to the sale.\n\n"
+      f"{buyer_name}, remember to delete your listing "
+      f"<a href='https://swipemarketcu.com'>here</a> once you've agreed to the sale.\n\n"
       "Best regards,\n"
       "Swipe Market Team"
     )
@@ -550,7 +551,8 @@ def send_connection_email():
       f"${price_str}. "
       f"{seller_name} accepts {seller_listing.payment_methods}.\n\n"
       f"{seller_name}, if this is the only swipe you want to sell today, "
-      "remember to delete your listing once you've agreed to the sale.\n\n"
+      f"remember to delete your listing <a href='https://swipemarketcu.com'>here</a> "
+      f"once you've agreed to the sale.\n\n"
       "Best regards,\n"
       "Swipe Market Team"
     )
