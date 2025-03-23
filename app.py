4619bcf8664133ed7e8f4cdb91731562d81d31bc
otenwerry <otenwerry@gmail.com>
2025-03-23 23:52:55 +0000

    added space to email

--- app.py
+++ app.py
@@ -664,7 +664,7 @@ def send_connection_email():
       f"${price_str}. "
       f"{seller_name} accepts {seller_listing.payment_methods}.\n\n"
       f"{seller_name}, if this is the only swipe you want to sell from this listing, "
-      f"remember to delete your listing <a href='https://swipemarketcu.com/?auto_delete={listing_id}'>here</a>"
+      f"remember to delete your listing <a href='https://swipemarketcu.com/?auto_delete={listing_id}'>here</a> "
       f"once you've agreed to the sale.\n\n"
       "Best regards,\n"
       "Swipe Market Team"
