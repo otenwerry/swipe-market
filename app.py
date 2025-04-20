dcf8369f3b7492d66801bcfcd980dc6a02986a43
otenwerry <otenwerry@gmail.com>
2025-04-20 13:52:59 +0200

    csrf (except javascript, not sure if necessary, checking now)

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ from  flask_sqlalchemy import SQLAlchemy
 from flask import make_response, g, render_template, flash
 from flask_mail import Mail, Message
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__) #sets up a flask application
+csrf = CSRFProtect(app)
 app.secret_key = os.environ.get('SECRET_KEY','fallback-secret-key') 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') # for PostgreSQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
