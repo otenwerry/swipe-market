e56747a862e20ee65048609e6d5ee62934adb4e3
Caroline Smyth <caroline.smyth@gmail.com>
2025-11-12 17:42:18 -0500

    new email sending

--- app.py
+++ app.py
@@ -30,12 +30,12 @@ app.secret_key = os.environ.get('SECRET_KEY','fallback-secret-key')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') # for PostgreSQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+"""app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
 app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT'))
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_DEFAULT_SENDER'] = 'trades@swipemarketcu.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')"""
 app.config.update(
     SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_HTTPONLY=True, 
